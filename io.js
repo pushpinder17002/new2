a757759b79293d83dc8249dde4d0eaa7bc4e5ad7
pushpinder17002 <pushpinder17002@gmail.com>
2024-09-21 12:47:36 +0000

    pratice done

--- io.js
+++ io.js
@@ -1,1 +1,26 @@
-console.log("helllo")
+/*
+"use strict";//use for newer veriosn of code
+let name="abc"// datatypes 
+let age=18// number
+let isloggedin=false//bool datatype
+// numbers= 2 to power of 54
+// bigint
+//string=""
+//null standalone value
+console.log(typeof(isloggedin))
+console.log(typeof(null))
+"33"=33 We can convert string into number
+"33abc"=NAN(Not a number) this string cannot be converted into number as there alphabet present 
+true=1,false=0
+""=false
+"abc"=true 
+*/
+// let score="33"
+// console.log(typeof(score))
+// let value_in_number=Number(score)
+// console.log(typeof(value_in_number))
+
+let number=34
+let string_number=String(number)
+console.log(string_number)
+console.log(typeof(string_number))
